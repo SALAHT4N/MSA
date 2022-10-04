d57e12087af7a9f99d0c37b9e65f6640b7a690b3
SALAHT4N <salah.aldin.tanbour@outlook.com>
2022-10-04 16:38:40 +0300

    signup first

--- src/main/java/com/software/mas/controller/login/SignUpController.java
+++ src/main/java/com/software/mas/controller/login/SignUpController.java
@@ -1,4 +1,49 @@
 package com.software.mas.controller.login;
 
-public class SignUpController {
+import com.software.mas.App;
+import javafx.css.Stylesheet;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignUpController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+    @FXML
+    private VBox customerCard;
+
+    @FXML
+    private VBox providerCard;
+
+    @FXML
+    void selectUserType(MouseEvent event) {
+        clearStyles();
+        int state = 0;
+        state = ((VBox)(event.getSource()) == providerCard) ? 1 : 2;
+
+        if (state == 1) {
+            providerCard.getStylesheets().add(App.class.getResource("/com/software/mas/CSS/signup/sign-ip--selected.css").toString());
+        }
+        else {
+            customerCard.getStylesheets().add(App.class.getResource("/com/software/mas/CSS/signup/sign-ip--selected.css").toString());
+        }
+
+    }
+    private void clearStyles() {
+        customerCard.getStylesheets().removeAll(customerCard.getStylesheets());
+        providerCard.getStylesheets().removeAll(providerCard.getStylesheets());
+    }
+
+
+
 }
